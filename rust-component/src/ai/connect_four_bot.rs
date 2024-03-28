3c788927147eb6de3b96faba3859185a7bf5c87d
Crystal Zhang <crystal7@ualberta.ca>
2024-03-27 23:49:28 -0600

    adding ying's stuff

---
+++ rust-component/src/ai/connect_four_bot.rs
@@ -0,0 +1,108 @@
+use crate::game_logic::connect_four::ConnectFour;
+use crate::game_logic::game_info::Message;
+use crate::game_logic::game_info::Player;
+
+use rand::prelude::*;
+
+pub struct ConnectFourBot {
+    game: ConnectFour,
+    num_sims: i32,
+}
+
+impl ConnectFourBot {
+    pub fn new(game: ConnectFour, num_sims: i32) -> Self {
+        return ConnectFourBot{ game, num_sims };
+    }
+
+    pub fn select_move(&mut self) -> usize {
+        /*
+        Selects the best move in a priority order:
+            1) Win if possible (no point blocking if win is possible)
+            2) Block opponent if needed
+            3) Choose best move by simulation
+        */
+        let scores = self.simulate();
+        let best_score_column = scores
+            .iter()
+            .enumerate()
+            .max_by_key(|(_, &value)| value)
+            .map(|(index, _)| index)
+            .unwrap();
+        
+        if self.check_win(Player::PlayerTwo, best_score_column) < self.game.board.max_cols {
+            return self.check_win(Player::PlayerTwo, best_score_column);
+        } 
+        else if self.check_win(Player::PlayerOne, best_score_column) < self.game.board.max_cols {
+            return self.check_win(Player::PlayerOne, best_score_column);
+        }
+        else {
+            return best_score_column;
+        }
+    }
+
+    fn check_win(&mut self, player: Player, bot_move: usize) -> usize {
+        // Iterate every position and check for win condition of player.
+        for column in 0..self.game.board.max_cols {
+            let mut game_clone = self.game.clone();
+
+            // player 2 makes best move and checks if it leads to player 1 wins
+            if player == Player::PlayerOne {
+                game_clone.play_move(bot_move);
+            }
+
+            let result = game_clone.play_move(column);
+            if result == Message::Winner(player) {
+                return column;
+            }
+        }
+        // no one will win next turn, return inpossible column
+        return self.game.board.max_cols + 1;
+    }
+  
+    fn simulate(&mut self) -> Vec<i64> {
+        // let mut scores: Vec<i64> = vec![0; self.game.board.max_cols];
+        let mut scores: Vec<i64> = vec![-4, -2, -2, -1, -2, -2, -4];
+
+        for _ in 0..self.num_sims {
+            for column in 0..self.game.board.max_cols {
+                // dummy message to initialize the variable
+                let mut result: Message = Message::NextPlayer(Player::PlayerTwo);
+                let mut game_clone = self.game.clone();
+
+                // based on Monte Carlo tree search: try a move and then play randomly to completion and repeat for every possible next move
+                result = game_clone.play_move(column);
+
+                // Give the highest penalty to a filled column so it's never chosen.
+                if result == Message::ColumnFull {
+                    scores[column] = -9223372036854775808;  
+                    continue;
+                }
+
+                loop {
+                    let mut rng = thread_rng();
+                    if rng.gen() {
+                        let bot_move: usize = rng.gen_range(0..(self.game.board.max_cols));
+                        result = game_clone.play_move(bot_move);
+                    }
+
+                    match result {
+                        Message::Winner(Player::PlayerOne) => {
+                            scores[column] -= 2;
+                            break;
+                        }
+                        Message::Winner(Player::PlayerTwo) => {
+                            scores[column] += 1;
+                            break;
+                        }
+                        Message::Tie => {
+                            scores[column] -= 1;
+                            break;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+        return scores;
+    }
+}
---
+++ rust-component/src/main.rs
@@ -0,0 +1,117 @@
+// mod web_assembly;
+// mod ai;
+// mod game_logic;
+
+// use std::io;
+// use ai::connect_four_bot::ConnectFourBot;
+
+// use crate::game_logic::connect_four::ConnectFour;
+// use crate::game_logic::game_info::{GameState, Message};
+// use crate::game_logic::game_info::Player;
+
+// fn main() {
+//     let mut game = ConnectFour::init(6, 7);
+//     let mut result = Message::NextPlayer(Player::PlayerOne);
+
+//     println!("\nYou are X, Bot is O");
+//     println!("{:?}", game);
+
+//     loop {
+//         println!("| 0 | 1 | 2 | 3 | 4 | 5 | 6 |\n");
+//         println!("your move: ");
+//         let mut ip = String::new();
+//         io::stdin().read_line(&mut ip).expect("Failed to read line\n");
+//         let ip: i8 = ip.trim().parse().expect("Invalid Input\n");
+
+//         println!("");
+
+//         result = game.play_move(ip as usize);
+//         println!("{:?}", game);
+
+//         if result == Message::Winner(Player::PlayerOne) || result == Message::Winner(Player::PlayerTwo) || result == Message::Tie {
+//             break;
+//         }
+
+//         // hard: 1000, medium: 500, easy: 5
+//         let mut mcst = ConnectFourBot::new(game.clone(), 1000);
+//         result = game.play_move(mcst.select_move());
+//         println!("{:?}", game);
+
+//         match result {
+//             Message::Winner(Player::PlayerOne) => {
+//                 println!("👎 BOO: you win");
+//                 break;
+//             }
+//             Message::Winner(Player::PlayerTwo) => {
+//                 println!("🥳 YAY: BOT WINS"); 
+//                 break;
+//             }
+//             Message::Tie => {
+//                 println!("BOO TIE");
+//                 break;
+//             }
+//             _ => {}
+//         }
+//     }
+// }
+
+mod web_assembly;
+mod ai;
+mod game_logic;
+
+use std::io;
+use ai::toot_otto_bot::TootOttoBot;
+
+use crate::game_logic::toot_otto::TootOtto;
+use crate::game_logic::game_info::{GameState, Message};
+use crate::game_logic::game_info::Player;
+
+fn main() {
+    let mut game = TootOtto::init(4, 6);
+    let mut result = Message::NextPlayer(Player::PlayerOne);
+
+    println!("{:?}", game);
+
+    loop {
+        println!("| 0 | 1 | 2 | 3 | 4 | 5 |\n");
+        println!("your move: ");
+        let mut ip = String::new();
+        io::stdin().read_line(&mut ip).expect("Failed to read line\n");
+        let args: Vec<&str> = ip.trim().split_whitespace().collect();
+        let col: usize = args[0].trim().parse().expect("Invalid Input\n");
+        let token: char = args[1].trim().parse().expect("Invalid Input\n");
+
+        println!("");
+
+        result = game.play_move(col, token);
+        println!("{:?}", game);
+
+        if result == Message::Winner(Player::PlayerOne) || result == Message::Winner(Player::PlayerTwo) || result == Message::Tie {
+            break;
+        }
+
+        // hard:, medium:, easy:
+        let mut mcst = TootOttoBot::new(game.clone(), 500);
+        let bot_move = mcst.select_move();
+        result = game.play_move(bot_move.0, bot_move.1);
+        println!("{:?}", game);
+        println!("{} {}", bot_move.0, bot_move.1);
+
+        if result == Message::Winner(Player::PlayerOne) || result == Message::Winner(Player::PlayerTwo) || result == Message::Tie {
+            break;
+        }
+    }
+
+    match result {
+        Message::Winner(Player::PlayerOne) => {
+            println!("👎 BOO: you win");
+        }
+        Message::Winner(Player::PlayerTwo) => {
+            println!("🥳 YAY: BOT WINS");
+        }
+        Message::Tie => {
+            println!("BOO TIE");
+        }
+        _ => {}
+    }
+}
