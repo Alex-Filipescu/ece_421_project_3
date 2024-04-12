0de70df334251a446cb556e875bb58f185b7d514
Crystal Zhang <crystal7@ualberta.ca>
2024-04-11 22:03:48 -0600

    add more instructions

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -10,7 +10,7 @@ pub fn home() -> Html {
             <h2 class = "welcome">{"Welcome!"}</h2>
             <h3>{"Connect 4 Instructions:"}</h3>
             <p>
-                {"First one to get 4 tokens in a row wins!"}
+                {"First one to get 4 tokens in a row (vertically, horizontally, diagonally) wins!"}
             </p>
             <p>
                 {"You will use the X token and play first, while the bot opponent will use the O token."}
@@ -20,7 +20,7 @@ pub fn home() -> Html {
 
             <h3>{"Toot Otto Intructions:"}</h3>
             <p>
-            {"First one to create TOOT or OTTO on the board wins!"}
+            {"First one to create TOOT or OTTO (vertically, horizontally, diagonally) on the board wins!"}
             </p>
             <p>
                 {"You will be trying to get TOOT, while the bot opponent will try to get OTTO."}
